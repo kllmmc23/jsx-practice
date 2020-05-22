37a273ee5ac15ae548dcd71ac88ee2ddf43b9d10
Michael <kllmmc23@gmail.com>
2020-05-21 20:17:43 -0600

    inline-styling-jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  return (
+    <div>
+      <label className="label" for=" name">Enter name </label>
+      <input id="name" type="text" />
+      <button>Submit</button>
+    </div>
+  );
+};
+ReactDOM.render(<App />, rootElement);
